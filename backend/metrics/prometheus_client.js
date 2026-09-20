e23056e79433766aa621c4b767107ebaf5fcbc30
Ludoowg <ludoowg@hotmail.fr>
2026-09-20 23:20:40 +0200

    clarify name for metrics

--- backend/metrics/prometheus_client.js
+++ backend/metrics/prometheus_client.js
@@ -35,14 +35,14 @@ const checkErrorsTotal = new client.Counter({
 });
 
 const httpRequestsTotal = new client.Counter({
-  name: "http_requests_total",
+  name: "monitoring_http_requests_total",
   help: "Total number of HTTP requests",
   labelNames: ["method", "route", "status_code"],
   registers: [register],
 });
 
 const httpRequestDuration = new client.Histogram({
-  name: "http_request_duration_seconds",
+  name: "monitoring_http_request_duration_seconds",
   help: "Duration of HTTP requests in seconds",
   labelNames: ["method", "route", "status_code"],
   buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5],
